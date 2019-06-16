316e12f4ef1eb45ebc020d135116bd6c7ee06d6d
Akhila Seneviratne <akhila_sene@yahoo.com>
2019-06-16 21:24:58 +0530

    commit

--- app/components/Dashboard/index.js
+++ app/components/Dashboard/index.js
@@ -2,18 +2,18 @@
 import { View, Text, Button} from 'react-native'
 
 
-class Dashboard extends Component {
+export default class Dashboard extends Component {
 
     render() {
         return (
         <View>
-            
+
             <Text>This is Dashboard</Text>
             
             <Button color='#454549' title={"register"} onPress={_ =>this.props.navigation.navigate('UserReg')} />
+       
         </View>
         )
     }
 }
 
- export default Dashboard 
